0f57d84287ce24eced3ce7b37295b1c1e69d1878
Ram Samala <RSamala@my.harrisburgu.edu>
2015-07-15 02:58:14 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿angular.module('RoutingApp', ['ngRoute'])
+	.config(['$routeProvider', function ($routeProvider) {
+	    $routeProvider
+			.when('/about', {
+			    templateUrl: 'about.html'
+			})
+			.when('/contact', {
+			    templateUrl: 'contact.html'
+			})
+			.otherwise({
+			    templateUrl: 'home.html'
+			});
+	}]);
